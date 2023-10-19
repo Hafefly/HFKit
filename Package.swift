dd4658dca60a561ecde1b99ad04d81853e7550e0
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-10-19 16:58:17 +0100

    switched to package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "HFKit",
-    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "HFKit", targets: ["HFKit"]),
+        .library(
+            name: "HFKit",
+            targets: ["HFKit"]),
     ],
     dependencies: [
         .package(path: "HFAuth"),
@@ -19,13 +20,17 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "HFKit",
+        .target(
+            name: "HFKit",
             dependencies: [
                 "HFAuth",
                 "HFCoreModel",
                 "HFCoreUI",
                 "HFNavigation"
             ]
-        )
+        ),
+        .testTarget(
+            name: "HFKitTests",
+            dependencies: ["HFKit"]),
     ]
 )
